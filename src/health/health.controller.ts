5452ab3abb667ed4bb17fef872ce631da3c61127
siraom15 <siriwatjaiyungyuen@gmail.com>
2023-08-19 14:47:19 +0700

    Update health

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheckService, HttpHealthIndicator, HealthCheck } from '@nestjs/terminus';
+import { Public } from 'src/auth/decorator/public.decorator';
 
-@Controller('health')
-export class HealthController {}
+@Controller('/api/health')
+@Public()
+export class HealthController {
+    constructor(
+        private health: HealthCheckService,
+        private http: HttpHealthIndicator,
+    ) { }
+
+    @Get()
+    @HealthCheck()
+    check() {
+        return this.health.check([
+            () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+        ]);
+    }
+}
