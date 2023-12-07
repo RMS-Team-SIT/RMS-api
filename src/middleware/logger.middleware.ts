4fc24d75aeaa4de4a7a996b74216bdb4307c2fb3
siraom15 <siriwatjaiyungyuen@gmail.com>
2023-12-07 19:59:23 +0700

    upd

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,17 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function logger(req: Request, res: Response, next: NextFunction) {
-  console.log(`----------------------------------------`);
   const { method, url, headers, body } = req;
-
   console.log(`Request ${method} ${url}`);
-  console.log('Headers:');
-  console.log(headers);
+  // console.log('Headers:');
+  // console.log(headers);
 
-  if (body) {
-    console.log('Body:');
-    console.log(body);
-  }
-  console.log(`----------------------------------------`);
+  // if (body) {
+  //   console.log('Body:');
+  //   console.log(body);
+  // }
   next();
 }
